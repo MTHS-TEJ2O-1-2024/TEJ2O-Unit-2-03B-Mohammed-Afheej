1ac071a6df9ae64b7ea706476d6dddf40649e349
Mohammed-Afheej <mohammed.afheej@mths.ca>
2024-11-11 20:00:53 -0500

    edited the comment

--- main.ts
+++ main.ts
@@ -1,8 +1,24 @@
 /* Copyright (c) 2020 MTHS All rights reserved
  *
- * Created by: Mr. Coxall
+ * Created by: Nov 2024
  * Created on: Sep 2020
- * This program ...
+ * This program This program does basic math
 */
 
-basic.showString('Hello, World!')
+basic.clearScreen()
+basic.pause(1000)
+
+basic.showString('A rectangle has dimensions 5 cm & 3 cm.')
+basic.pause(500)
+
+// multiply
+basic.showString('The perimeter would be: ' + (2 * (5 + 3)).toString() + 'cm.')
+basic.showIcon(IconNames.Happy)
+basic.pause(500)
+basic.clearScreen()
+
+// multiply
+basic.showString('The area would be: ' + (5 * 3).toString() + 'cm^2.')
+basic.showIcon(IconNames.Happy)
+basic.pause(500)
+basic.clearScreen()
